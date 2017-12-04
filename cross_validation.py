660f9cef63e987937e38c123e9108f590407756b
kulka193 <30439795+kulka193@users.noreply.github.com>
2017-12-04 00:12:37 -0600

    Update cross_validation.py

--- cross_validation.py
+++ cross_validation.py
@@ -11,24 +11,3 @@ def Kcross(X,y,indexlist):
     train_data=np.delete(X,indexlist,axis=0)
     train_target=np.delete(y,indexlist,axis=0)
     return(train_data,test_data,train_target,test_target)
-
-
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
-#    m=np.size(X,0)
-#    n=np.size(X,1)
-#    f=int(train_percent*m)
-#    indices=sample(range(m),f)
-#    train_data=X[indices]
-#    test_data=np.delete(X,indices,0)
-#    train_target=t[indices]
-#    test_target=np.delete(t,indices)
-#    return(train_data,test_data,train_target,test_target)
